f49512a9a0628680bb9f84bd6f6022b13d2a2f0e
Aleksey Drobnych <adrobnych@gmail.com>
2013-06-25 21:31:26 +0300

    QuizTestPrinterSpec#itShouldPrintQuestionOfSingleTest GREEN

--- src/com/tdl/annotated_quiz/annotations/Question.java
+++ src/com/tdl/annotated_quiz/annotations/Question.java
@@ -1,5 +1,9 @@
 package com.tdl.annotated_quiz.annotations;
 
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface Question {
 
 }
--- src/com/tdl/annotated_quiz/runners/QuizTestPrinter.java
+++ src/com/tdl/annotated_quiz/runners/QuizTestPrinter.java
@@ -1,5 +1,9 @@
 package com.tdl.annotated_quiz.runners;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import com.tdl.annotated_quiz.annotations.Question;
 import com.tdl.annotated_quiz.quiz_tests.QuizTest;
 
 public class QuizTestPrinter {
@@ -10,7 +14,26 @@ public class QuizTestPrinter {
 	}
 
 	public String start() {
-		return null;
+		String result = null;
+		for (Method m : test.getClass().getDeclaredMethods()) {
+			if (m.isAnnotationPresent(Question.class)) {
+				try {
+					result = "" + m.invoke(test, (Object[])null);
+				} catch (InvocationTargetException wrappedExc) {
+					Throwable exc = wrappedExc.getCause();
+					System.out.println(m + " failed: " + exc);
+				} catch (Exception exc) {
+					System.out.println("INVALID @Code: " + m);
+				}
+			}
+		}
+		return result;
+	}
+	
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+		Class<?> testClass = Class.forName(args[0]);
+		QuizTestPrinter runner = new QuizTestPrinter( (QuizTest) testClass.newInstance() );
+		System.out.println(runner.start());
 	}
 	
 }
